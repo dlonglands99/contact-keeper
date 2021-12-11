a55340036dff6d71f0b22967ba719f4091c982aa
Dylan <d.longlands99@gmail.com>
2021-12-11 19:59:40 +0000

    Add navbar, about page and initial routing

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About this App</h1>
+            <p className="my-1">Full stack MERN Application for storing contacts</p>
+            <p className="bg-dark p">Version: <strong>1.0.0</strong></p>
+        </div>
+    )
+};
+
+export default About;
